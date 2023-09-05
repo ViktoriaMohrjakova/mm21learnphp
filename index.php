2f67fc0880be65c1abd955c1cd3a18ea51d9ce18
Kaspar Martin Suursalu <kasparsu@gmail.com>
2023-09-05 11:30:04 +0300

    basic site

--- index.php
+++ index.php
@@ -1,51 +1,13 @@
 <?php
+$name = 'Kaspar';
+$num = 10;
+?>
 
-class Box {
-    protected $length;
-    protected $height;
-    protected $width;
-    protected $material;
-    public static $count = 0;
-    public function __construct($length, $height, $width){
-        $this->length = $length;
-        $this->width = $width;
-        $this->height = $height;
-    }
-    
-    public function volume(){
-        var_dump(self::$count);
-        return $this->length * $this->height * $this->width;
-    }
-
-    public function getLength(){
-        return $this->length;
-    }
-    public function setLength($length){
-        if($length > 0){
-            $this->length = $length;
-        }
-    }
-    public static function countUp(){
-        self::$count++;
-    }
-}
-
-class MetalBox extends Box {
-    public $weight;
-    public function __construct($length, $height, $width){
-        $this->length = $length;
-        $this->width = $width;
-        $this->height = $height;
-        $this->material = 'metal';
-    }
-}
-
-$box = new MetalBox(1,1,1);
-$box->volume();
-MetalBox::countUp();
-
-
-MetalBox::countUp();
-
-MetalBox::countUp();
-var_dump(MetalBox::$count);
+<?php include 'partials/header.php'; ?>
+    <h1><?=$name?></h1>
+    <?php if($num > 10): ?>
+         <h1>Suurem</h1>
+    <?php else: ?>
+        <h1>Väiksem</h1>
+    <?php endif ?>
+<?php include 'partials/footer.php'; ?>
