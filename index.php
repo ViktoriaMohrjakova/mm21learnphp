952e5bcda1b52a35925fb96aad76852c5e1f5b0c
Viktoria Mohrjakova <viktoriamohrjakova@gmail.com>
2023-09-05 11:31:37 +0300

    basic site

--- index.php
+++ index.php
@@ -1,51 +1,15 @@
 <?php
+$name='Tori';
+$num=10;
 
-class Box {
-    protected $length;
-    protected $height;
-    protected $width;
-    protected $material;
-    public static $count = 0;
-    
-    public function __construct($length, $height, $width){
-        $this->length = $length;
-        $this->width = $width;
-        $this->height = $height;
-    }
-    
-    public function volume(){
-        var_dump(self::$count);
-        return $this->length * $this->height * $this->width;
-    }
-    public function getLength(){
-        return $this-> length;
-    }
-    public function setLength(){
-        if($length > 0){
-            $this->length = $length;
-        }
-    }
-    public static function countup(){
-        self:: $count++;
-    }
-}
+?>
 
-class MetalBox extends Box {
-   public $weight;
-   public function __construct($length, $height, $width){
-        $this->length = $length;
-        $this->width = $width;
-        $this->height = $height;
-        $this-> material = 'metal';
-   }
-}
-
-$box= new MetalBox(1,1,1);
-$box->volume();
-MetalBox::countup();
-
-MetalBox::countup();
-
-MetalBox::countup();
-
-var_dump(MetalBox::$count);
+<?php include 'partials/header.php'; ?>
+<?php include 'partials/nav.php'; ?>
+    <h1><?=$name ?> <h1>
+    <?php if($num>10): ?>
+        <h1>Suurem<h1>
+     <?php  else : ?>
+         <h1> Väiksem <h1>
+       <?php endif ?>
+       <?php include 'partials/footer.php'; ?>
---
+++ partials/nav.php
@@ -0,0 +1,4 @@
+<ul>
+    <li><a href="/" >Home Pages</a> </li>
+      <li><a href="about.php">About </a> </li>
+</ul>
