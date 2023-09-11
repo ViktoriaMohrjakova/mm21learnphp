5a1bd3d143c80d4610ba2f7c6625e0437ea4d26a
Kaspar Martin Suursalu <kasparsu@gmail.com>
2023-09-11 15:08:48 +0300

    Add controllers

--- public/index.php
+++ public/index.php
@@ -3,29 +3,42 @@
 spl_autoload_register(function ($className){
     
     $className = substr($className, 4);
-    //var_dump($className);
+   
     require_once __DIR__ . "/../src/$className.php";
 });
 
+use App\Router;
 
-//
-use App\Animals\Dog as Doggo;
+require __DIR__ . '/../routes.php';
 
-$router = new App\Router();
-var_dump(new Doggo());
+$router = new Router($_SERVER['REQUEST_URI']);
+$match = $router->match();
+if($match){
+    if(is_callable($match['action'])){
+        call_user_func($match['action']);
+    } else if(is_array($match['action'])){
+        $class = $match['action'][0]; // 'App\Controllers\PublicController'
+        $controller = new $class();
+        $method = $match['action'][1];
+        $controller->$method();
+    }
+} else {
+    echo 404;
+}
+
 
 
 
 
 //var_dump($_SERVER);
-switch ($_SERVER['REQUEST_URI']) {
-    case '/':
-        echo 'Home page';
-        break;
-    case '/about':
-        echo 'About';
-        break;
-    default:
-        echo '404';
-        break;
-}
+// switch ($_SERVER['REQUEST_URI']) {
+//     case '/':
+//         echo 'Home page';
+//         break;
+//     case '/about':
+//         echo 'About';
+//         break;
+//     default:
+//         echo '404';
+//         break;
+// }
